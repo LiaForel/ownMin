f944ad712bc4ac9675993c6a1bbf54101abf58ae
LiaForel <bkurmant@mail.sfsu.edu>
2020-04-28 17:49:27 -0700

    Initial Commit

---
+++ ownMin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ownMin
+//
+//  Created by Bakulia Kurmant on 4/28/20.
+//  Copyright © 2020 Bakulia Kurmant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
